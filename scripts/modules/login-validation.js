7e616cffc559a19d0c80f6b8759ac2fef139b6ad
Rodolfo Aldrovandi <rodolfoaldrovandip@gmail.com>
2021-05-19 11:55:57 -0400

    últimas alterações

--- scripts/modules/login-validation.js
+++ scripts/modules/login-validation.js
@@ -1,49 +1,45 @@
-import {configs} from './configsApi.js';
+import { configs } from './configsApi.js';
 
 export default function initLogin() {
+  const btnEntrar = document.querySelector('#btnEntrar');
+  const loginMensagem = document.querySelector('.loginMensagem');
+  const cadastroSugestao = document.querySelector('.cadastroSugestao');
 
-    const btnEntrar = document.querySelector('#btnEntrar');
-    const loginMensagem = document.querySelector('.loginMensagem');
+  function getDataLogin() {
+    loginMensagem.innerHTML = '';
+    const seletorTipoPessoa = document.querySelector('#seletorTipoPessoaLogin');
+    const login = document.querySelectorAll('[data-login]');
 
-    function getDataLogin() {
-        
-        loginMensagem.innerHTML = '';
-        const seletorTipoPessoa = document.querySelector('#seletorTipoPessoaLogin');
-        const login = document.querySelectorAll('[data-login]');
-            
-        validationLogin(login[0].value, login[1].value, seletorTipoPessoa.value);
+    validationLogin(login[0].value, login[1].value, seletorTipoPessoa.value);
+  }
+
+  btnEntrar.addEventListener('click', getDataLogin);
+
+  async function validationLogin(email, password, typePerson) {
+    const json = JSON.stringify({
+      email: email,
+      password: password,
+      typePerson: typePerson,
+    });
+    const response = await fetch(`${configs.baseUrl}/session`, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: json,
+    });
+    const responseLogin = await response.json();
+
+    if (response.status == 401) {
+      loginMensagem.innerHTML = responseLogin.error;
+      cadastroSugestao.innerHTML = responseLogin.sugestao;
+    } else {
+      loginMensagem.innerHTML = responseLogin.ok;
+      setInterval(() => {
+        window.location.reload();
+      }, 1000);
+      const dataUser = JSON.stringify(responseLogin);
+      sessionStorage.setItem('dataUser', dataUser);
     }
-
-    btnEntrar.addEventListener('click', getDataLogin);
-
-    async function validationLogin(email, password, typePerson) {
-        
-        const json = JSON.stringify({
-            email: email,
-            password: password,
-            typePerson: typePerson
-        })
-        const response = await fetch(`${configs.baseUrl}/session`, {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json'
-            },
-            body: json
-        })
-        const responseLogin = await response.json();
-
-        if(response.status == 401){
-            loginMensagem.innerHTML = responseLogin.error;
-        }else {
-            loginMensagem.innerHTML = responseLogin.ok;
-            setInterval(() => {
-                window.location.reload();
-            }, 1000);
-            const dataUser = JSON.stringify(responseLogin);
-            sessionStorage.setItem('dataUser', dataUser);
-        }
-
-    }
-    
+  }
 }
-
